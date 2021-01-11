0a55ff0170377aee06cb9f3aadfbca7b46518319
Jinmin <leijinmin@gmail.com>
2021-01-10 19:59:47 -0500

    update instructions

--- delete_resources.ps1
+++ delete_resources.ps1
@@ -1,10 +1,11 @@
 # delete resources
-aws glue delete-job --job-name jinmin-daily_avg
-aws glue delete-database --name awsblogsgluedemo
-aws cloudformation delete-stack --stack-name aws-kinesis
+aws glue delete-job --job-name daily_avg --region us-east-1 --profile default
+aws glue delete-database --name awsblogsgluedemo --region us-east-1 --profile default
+aws glue delete-crawler --name thermostat-data-crawler --region us-east-1 --profile default
+aws cloudformation delete-stack --stack-name aws-kinesis --region us-east-1 --profile default
 
-aws iam detach-role-policy --role-name AWSGlueServiceRoleDefault --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
-aws iam detach-role-policy --role-name AWSGlueServiceRoleDefault --policy-arn arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole
-aws iam detach-role-policy --role-name AWSGlueServiceRoleDefault --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess
-aws iam delete-role --role-name AWSGlueServiceRoleDefault 
+aws iam detach-role-policy --role-name AWSGlueServiceRoleDefault --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --region us-east-1 --profile default
+aws iam detach-role-policy --role-name AWSGlueServiceRoleDefault --policy-arn arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole --region us-east-1 --profile default
+aws iam detach-role-policy --role-name AWSGlueServiceRoleDefault --policy-arn arn:aws:iam::aws:policy/CloudWatchLogsFullAccess --region us-east-1 --profile default
+aws iam delete-role --role-name AWSGlueServiceRoleDefault --region us-east-1 --profile default
 
